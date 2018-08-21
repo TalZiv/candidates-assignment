c9421d7fed3a852a4cfb7788431479b6b89ddd72
TalZiv <tal.ziv.w@gmail.com>
2018-08-21 22:26:10 +0300

    Solutions

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 #add fix to exercise1 here
+sudo route delete www.textfiles.com
+curl 'http://www.textfiles.com/art/bnbascii.txt'
--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 #add fix to exercise3 here
+# fix apache deny
+sudo sed -i '13s/deny from all/allow from all/' /etc/apache2/sites-enabled/000-default ; sudo service apache2 restart
+curl http://192.168.100.10
